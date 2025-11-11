61039492a12cc038679d55014b832af1ca5e7190
John Q. Herman <johnqherman@gmail.com>
2025-11-11 13:44:23 -0500

    fix cron syntax for poll interval

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ class Config {
         this.apiUrl =
             'https://api.raccoonlagoon.com/v1/server-info?ip=104.153.104.12:27015&g=tf2';
         this.webhookUrls = this.parseWebhookUrls();
-        this.pollInterval = '1 * * * *';
+        this.pollInterval = '*/1 * * * *';
         this.maxRetries = 3;
         this.retryDelay = 30 * 1000;
     }
